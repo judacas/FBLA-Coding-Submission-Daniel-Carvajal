d4577519871134d493bcfe510f1ba65dc5ab7fbd
Daniel Carvajal <dcarvajal@gmail.com>
2022-01-12 01:35:53 -0500

    Updated for submission

---
+++ README.txt
@@ -0,0 +1,10 @@
+Made by Daniel Carvajal From C. Leon King High School
+To run this program you will need to assure that you have a Java JDK installed (recommended version: java 10.0.2)
+You will also need to be sure that you have python3 installed (recommended version: 3.8.2) 
+NOTE: it must be python3 not python
+Within python you need to have the selenium library installed 
+to install go to command line and type "pip3 install selenium"
+should pip not be installed then type "curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py"
+then "python3 get-pip.py"
+to run the actual program cd (change directory) to the folder in which you downloaded the code (should be *./FBLA)
+then type "javac RunMe.java && java RunMe"      
--- TripAdvisorSearcher.py
+++ TripAdvisorSearcher.py
@@ -14,47 +14,27 @@ opt.add_argument("--disable-notifications")
 opt.add_argument("headless")
 opt.add_argument("start-maximized")
 opt.add_argument("--disable-extensions")
-# Pass the argument 1 to allow and 2 to block
 opt.add_experimental_option("prefs", { \
     "profile.default_content_setting_values.geolocation": 1, 
   })
 
 browser = webdriver.Chrome(options=opt)
-# browser = webdriver.Chrome()
-# query = "hotels near me"
 url = "https://www.tripadvisor.com/Search?q=" + query + "&blockRedirect=true"
-# print("searching for attractions...")
 browser.get(url)
-
-# alert = wait.until(expected_conditions.alert_is_present())
-
-# print("still searching for attractions...")
 time.sleep(2)
-# print("almost done...")
-
-# try:
-#     elements = browser.find_elements("result-title")
-# except BaseException:
-#     print("bruh")
-#     pass
 results = ""
 try:
     browser.find_element(By.CLASS_NAME, "original-query").click()
-    # print("aight we back to the oq!!!")
     time.sleep(1)
-    # browser.find_element(By.XPATH("//*[contains(text(),'Show more')]"));
 except BaseException:
     pass
 
 try:
     browser.find_element(By.CLASS_NAME, "result-section-footer-columns").click()
 except BaseException:
-    # print("not found via result-section-footer-columns")
     try:
         browser.find_element(By.CLASS_NAME, "show-block").click()
     except BaseException:
-        # print("not found via show-block")
-        # print("not found at all")
         pass
     pass
 
@@ -70,10 +50,7 @@ try:
                 results += span.text + "\n"
         except BaseException:
             pass
-    # finalResults = open("finalResults.txt", "w")
-    # finalResults.write(results + ";")
-    # time.sleep(10)
-    # browser.close()   
+    browser.close()   
 except BaseException:
     print("Sorry, couldn't find any results that matched your request\n please try again with different attributes")
     finalResults = open("finalResults.txt", "w")
